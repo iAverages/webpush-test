a73d18422fb7084a4fe1f76571b4881ba150343f
iAverages <31937542+iAverages@users.noreply.github.com>
2023-11-30 13:57:13 +0000

    fix error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -830,6 +830,20 @@
     </style>
 
     <script>
+        function urlBase64ToUint8Array(base64String) {
+            var padding = '='.repeat((4 - base64String.length % 4) % 4);
+            var base64 = (base64String + padding)
+                .replace(/\-/g, '+')
+                .replace(/_/g, '/');
+
+            var rawData = window.atob(base64);
+            var outputArray = new Uint8Array(rawData.length);
+
+            for (var i = 0; i < rawData.length; ++i) {
+                outputArray[i] = rawData.charCodeAt(i);
+            }
+            return outputArray;
+        }
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', function() {
                 navigator.serviceWorker.register('sw.js');
@@ -845,10 +859,11 @@
                                 // Get the server's public key
                                 const response = await fetch('./vapidPublicKey');
                                 const vapidPublicKey = await response.text();
+                                const paddedKey = urlBase64ToUint8Array(vapidPublicKey);
 
                                 return registration.pushManager.subscribe({
                                     userVisibleOnly: true,
-                                    applicationServerKey: vapidPublicKey
+                                    applicationServerKey: paddedKey
                                 });
                             });
                     }).then(function(subscription) {
@@ -859,7 +874,7 @@
                                 'Content-type': 'application/json'
                             },
                             body: JSON.stringify(
-                                 subscription
+                                subscription
                             ),
                         });
 
